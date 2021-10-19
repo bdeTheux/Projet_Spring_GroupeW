b9e624e0dc2a9282c23821db559dcc016bb58769
Verstappen_bastien <bastien.verstappen@outlook.com>
2021-10-19 10:19:02 +0200

    add - back and front basket(initializer)

---
+++ BackendBasket/src/test/java/Backend/Backendbasket/BackendbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.Backendbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
