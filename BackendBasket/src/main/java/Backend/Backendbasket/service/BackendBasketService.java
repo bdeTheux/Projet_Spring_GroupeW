5882abc3f63848797a574a5adaf7c449259732dd
Verstappen_bastien <Bastien.verstappen@outlook.be>
2021-11-09 10:29:19 +0100

    add and refactor - frontend,backend

--- BackendBasket/src/main/java/Backend/Backendbasket/service/BackendBasketService.java
+++ BackendBasket/src/main/java/Backend/Backendbasket/service/BackendBasketService.java
@@ -1,6 +1,6 @@
 package Backend.Backendbasket.service;
 
-import Backend.Backendbasket.modele.BackendBasket;
+import Backend.Backendbasket.model.BackendBasket;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,8 +11,12 @@ public class BackendBasketService {
 
     private List<BackendBasket> baskets = new ArrayList<>();
 
-    public List<BackendBasket> findAll(){
-        return baskets;
+    public List<BackendBasket> findAll(int id_user){
+        List<BackendBasket> usersBaskets = new ArrayList<>();
+        for (BackendBasket bask : baskets) {
+            if (bask.getId_user() == id_user) usersBaskets.add(bask);
+        }
+        return usersBaskets;
     }
 
     public BackendBasket findById(int id){
---
+++ FrontendBasket/src/main/java/Frontend/FrontendBasket/model/FrontendBasket.java
@@ -0,0 +1,11 @@
+package Frontend.FrontendBasket.model;
+
+import lombok.Data;
+
+
+@Data
+
+public class FrontendBasket {
+
+    private int id , id_user,id_product, quantity;
+}
