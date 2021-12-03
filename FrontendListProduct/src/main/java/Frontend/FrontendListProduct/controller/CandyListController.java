552a4923185a7a5d10084c4a31e01c88f5c0c91d
bdeTheux <boris.detheux@student.vinci.be>
2021-12-04 00:51:18 +0100

    Ajout d'un service dans le front

--- FrontendListProduct/src/main/java/Frontend/FrontendListProduct/controller/CandyListController.java
+++ FrontendListProduct/src/main/java/Frontend/FrontendListProduct/controller/CandyListController.java
@@ -6,6 +6,7 @@ import Frontend.FrontendListProduct.model.CandyDTO;
 import Frontend.FrontendListProduct.model.Category;
 import Frontend.FrontendListProduct.model.PriceRange;
 import Frontend.FrontendListProduct.proxy.CandyProxy;
+import Frontend.FrontendListProduct.service.CandyService;
 import com.ctc.wstx.shaded.msv_core.driver.textui.Debug;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,16 +24,16 @@ import java.util.stream.Collectors;
 //@RequestMapping("/candies")
 @Controller
 public class CandyListController {
-    private CandyProxy proxy;
-    public CandyListController(CandyProxy proxy){
-        this.proxy = proxy;
+    private CandyService service;
+    public CandyListController(CandyService service){
+        this.service = service;
     }
 
     @GetMapping("/candies")
     public String candies(@RequestParam(required = false)String category, @RequestParam(required = false)String order, @RequestParam(required = false)Double min, @RequestParam(required = false)Double max,Model model){
         //J'ai mis cette condition car je ne trouve pas comment enlever le 0.0 de base dans le input
         if(min != null && max != null && min == 0.0 && max == 0.0) max = null;
-        List<Candy> candiesList = proxy.findAll(findCategory(category), order, min, max);
+        List<Candy> candiesList = service.findAll(service.findCategory(category), order, min, max);
         List<String> orderList = new ArrayList<String>(Arrays.asList("none", "asc", "desc"));
         System.out.println("order :" + order);
         System.out.println("min: " + min);
@@ -41,31 +42,13 @@ public class CandyListController {
         model.addAttribute("orders", orderList);
         model.addAttribute("candies", candiesList);
        // model.addAttribute("candies", category==null ? listCandies(): findByCategory(category));
-        model.addAttribute("categories", getCategories());
+        model.addAttribute("categories", service.getCategories());
         model.addAttribute("candy", new CandyDTO());
         return "candies";
     }
 
-    public Category findCategory(String category){
-        for(Category cat: Category.values()){
-            if(cat.getName().equals(category)){
-                return cat;
-            }
-        }
-        return null;
-    }
 
-    public List<Category> getCategories(){
-        List<Category> cat = new ArrayList<>();
-        for(Category c : Category.values()){
-            cat.add(c);
-        }
-        return cat;
-    }
 
-    public List<Candy> listCandies(){
-        return proxy.findAll(null, null, null, null);
-    }
 
     @PostMapping("/candies")
     public ModelAndView createCandy(@ModelAttribute CandyDTO candy, @RequestParam(name="cat", required = false) String category) {
@@ -82,7 +65,7 @@ public class CandyListController {
             cand.setCategory(Category.PHYSICS);
         }
 
-        proxy.saveCandy(cand);
+        service.saveCandy(cand);
         return new ModelAndView("redirect:/candies");
     }
 
@@ -99,13 +82,13 @@ public class CandyListController {
         }else{
             cdy.setCategory(Category.PHYSICS);
         }
-        proxy.updateCandy(id, cdy);
+        service.updateCandy(id, cdy);
         return new ModelAndView("redirect:/candies");
     }
 
     @GetMapping("/candies/update/{id}")
     public String updateCandy(@PathVariable("id")int id, Model model){
-        Candy candy = proxy.findById(id);
+        Candy candy = service.findById(id);
         CandyDTO candyDTO = new CandyDTO();
 
         candyDTO.setId(candy.getId());
@@ -115,7 +98,7 @@ public class CandyListController {
         candyDTO.setPrice(candy.getPrice());
         candyDTO.setCategory(candy.getCategory());
         model.addAttribute("candy", candyDTO);
-        model.addAttribute("categories", getCategories());
+        model.addAttribute("categories", service.getCategories());
         return "updateCandy";
     }
 
@@ -123,7 +106,7 @@ public class CandyListController {
     @PostMapping("/candies/delete/{id}")
     public ModelAndView deleteCandy(@PathVariable("id") int id){
         System.out.println("je suis ici");
-        proxy.deleteCandy(id);
+        service.deleteCandy(id);
         return new ModelAndView("redirect:/candies");
     }
     @GetMapping("/candy/{id}")
---
+++ FrontendListProduct/src/main/java/Frontend/FrontendListProduct/service/CandyService.java
@@ -0,0 +1,58 @@
+package Frontend.FrontendListProduct.service;
+
+
+import Frontend.FrontendListProduct.model.Candy;
+import Frontend.FrontendListProduct.model.Category;
+import Frontend.FrontendListProduct.proxy.CandyProxy;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CandyService {
+
+    private CandyProxy proxy;
+    public CandyService(CandyProxy proxy){
+        this.proxy = proxy;
+    }
+
+    public Category findCategory(String category){
+        for(Category cat: Category.values()){
+            if(cat.getName().equals(category)){
+                return cat;
+            }
+        }
+        return null;
+    }
+
+    public List<Category> getCategories(){
+        List<Category> cat = new ArrayList<>();
+        for(Category c : Category.values()){
+            cat.add(c);
+        }
+        return cat;
+    }
+    public List<Candy> findAll(Category category, String order, Double min, Double max){
+        return proxy.findAll(category, order, min, max);
+    }
+
+    public void saveCandy(Candy candy){
+        proxy.saveCandy(candy);
+    }
+
+    public void updateCandy(int id, Candy candy){
+        proxy.updateCandy(id, candy);
+    }
+    public Candy findById(int id){
+        return proxy.findById(id);
+    }
+
+    public void deleteCandy(int id){
+        proxy.deleteCandy(id);
+    }
+
+}
